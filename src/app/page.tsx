ce8bfeefba4af17b4a8a15c4789aee3efdab5008
Obed ADIFON <135321674+obedsallai@users.noreply.github.com>
2024-05-18 20:27:38 +0200

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 "use client"
-import Image from "next/image"
 import Button from "@/components/button"
-import FormStyle from "@/components/formStyle"
 import Link from "next/link"
 import { useState } from "react"
 export default function Formulaire(){
   const rounded='rounded-lg p-4'
   const mytext = 'text-lg '
-  const divPad='p-4'
   const [formData, setFormData]=useState({
     numero:'',
     societe:'',
@@ -24,11 +21,11 @@ export default function Formulaire(){
     email:false,
     montant:false
   });
-  const handleChange=(e)=>{
+  const handleChange=(e:any)=>{
     const {name,value}=e.target;
     setFormData({...formData, [name]:value});
   };
-  const handleSubmit=(e)=>{
+  const handleSubmit=(e:any)=>{
     e.preventDefault()
     const{numero, societe,nom,prenom,email,montant} = formData
 
@@ -115,8 +112,8 @@ export default function Formulaire(){
               <div className='p-5 mr-4 ml-4 w-9/12'>
                 <label className={mytext} htmlFor="facture">Ajouter la Facture</label>
                 <div className="w-full rounded-lg flex h-20 items-center flex-col  bg-gray-200 border-2 border-blue-700 border-dashed">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
                   </svg>
                   <p className="">Glisser et Déposer un fichier ou <Link className="text-blue-600" href={'/'}>Téléverser</Link></p>
                   
