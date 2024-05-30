8f59ba90494ef1b4e1b42fd4e6f7ad70b249658a
obedsallai <adifonobedsallai@gmail.com>
2024-05-30 12:50:30 +0200

    new changes

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -27,7 +27,7 @@ export default function Table(){
                                 </div></td>
                             <td className=" montant">€53,18</td>
                             <td className=" text-center"><Stat><p className="en_attente">En attente</p></Stat></td>
-                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                 </svg><Link href={'/'}><p className="voir_facture">Voir la facture</p></Link>
@@ -42,7 +42,7 @@ export default function Table(){
                                 </div></td>
                             <td className=" montant">€53,18</td>
                             <td className=" text-center"><Statt><p className="envoye">Envoyé</p></Statt></td>
-                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg><Link href={'/'}><p className="voir_facture">Voir la facture</p></Link>
@@ -57,7 +57,7 @@ export default function Table(){
                                 </div></td>
                             <td className=" montant">€53,18</td>
                             <td className=" text-center"><Statt><p className="envoye">Envoyé</p></Statt></td>
-                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg><Link href={'/'}><p className="voir_facture">Voir la facture</p></Link>
@@ -72,7 +72,7 @@ export default function Table(){
                                 </div></td>
                             <td className=" montant">€53,18</td>
                             <td className=" text-center"><Statt><p className="envoye">Envoyé</p></Statt></td>
-                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg><Link href={'/'}><p className="voir_facture">Voir la facture</p></Link>
@@ -87,7 +87,7 @@ export default function Table(){
                                 </div></td>
                             <td className=" montant">€53,18</td>
                             <td className=" text-center"><Statt><p className="envoye">Envoyé</p></Statt></td>
-                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-6 h-6">
+                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg><Link href={'/'}><p className="voir_facture">Voir la facture</p></Link>
@@ -102,7 +102,7 @@ export default function Table(){
                                 </div></td>
                             <td className=" montant">€53,18</td>
                             <td className=" text-center"><Statt><p className="envoye">Envoyé</p></Statt></td>
-                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg><Link href={'/'}><p className="voir_facture">Voir la facture</p></Link>
@@ -117,7 +117,7 @@ export default function Table(){
                                 </div></td>
                             <td className=" montant">€53,18</td>
                             <td className=" text-center"><Statt><p className="envoye">Envoyé</p></Statt></td>
-                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg><Link href={'/'}><p className="voir_facture">Voir la facture</p></Link>
@@ -132,7 +132,7 @@ export default function Table(){
                                 </div></td>
                             <td className=" montant">€53,18</td>
                             <td className=" text-center"><Statt><p className="envoye">Envoyé</p></Statt></td>
-                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <td className=" text-center"><button className="flex justify-between ml-20 border border-solid border-green-300 rounded-lg pl-2 pr-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                             </svg><Link href={'/'}> <p className="voir_facture">Voir la facture</p> </Link>
