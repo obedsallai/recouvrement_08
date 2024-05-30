dad932d60ab129097442b9fc8d1dac7b9ed54194
obedsallai <adifonobedsallai@gmail.com>
2024-05-30 11:42:04 +0200

    new changes at next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-<<<<<<< HEAD
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-=======
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
->>>>>>> 1d4ae3b6663ec2e2f165b9f2628e4124d9ba76e8
-};
+const nextConfig = {};
 
 export default nextConfig;
