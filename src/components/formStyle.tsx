61e83c215db6d9d81c23a42458e5b8035410996f
obedsallai <adifonobedsallai@gmail.com>
2024-05-30 13:13:45 +0200

    component error fixed

--- src/components/formStyle.tsx
+++ src/components/formStyle.tsx
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-
-const  FormStyle =({children}:{children: React.ReactNode}) =>{
-    return(
-        <div className="rounded-lg p-4 shadow-lg">
-            {children}
-        </div>
-    );
-}
-export default FormStyle;
-=======
 /*eslint-disable*/
 export default function FormStyle({children}:{children:React.ReactNode}){
     return 
@@ -16,4 +5,3 @@ export default function FormStyle({children}:{children:React.ReactNode}){
             {children}
     </div>
 }
->>>>>>> 1d4ae3b6663ec2e2f165b9f2628e4124d9ba76e8
