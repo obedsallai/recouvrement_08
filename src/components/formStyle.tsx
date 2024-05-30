73e531844da2b78c9bad8b9170747eb7b389e412
obedsallai <adifonobedsallai@gmail.com>
2024-05-30 13:56:14 +0200

    component formStyle has been removed from repository

--- src/components/formStyle.tsx
+++
@@ -1,7 +0,0 @@
-/*eslint-disable*/
-export default function FormStyle({children}:{children:React.ReactNode}){
-    return 
-    <div className='rounded-lg p-4 shadow-lg'>
-            {children}
-    </div>
-}
